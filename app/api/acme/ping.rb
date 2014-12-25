af9c3b46aaee302edf1747ef686daa68bc470331
Meck <yesmeck@gmail.com>
2014-12-25 22:36:41 +0800

    grape routes not found demo

--- app/api/acme/ping.rb
+++ app/api/acme/ping.rb
@@ -1,8 +1,11 @@
 module Acme
   class Ping < Grape::API
-    desc 'Returns pong.'
-    get :ping do
-      { ping: params[:pong] || 'pong' }
+    version 'v1'
+    resources :foos do
+      desc 'Returns pong.'
+      get :ping do
+        { ping: params[:pong] || 'pong' }
+      end
     end
   end
 end
